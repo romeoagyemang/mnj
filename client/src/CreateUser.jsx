c900cbaa670a8489db22f51eaf712bed1d7113dd
romeoagyemang <romeoasiamah79@gmail.com>
2023-12-25 00:42:39 -0600

    changes

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -22,7 +22,7 @@ function CreateUser() {
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
           <div className="w-50 bg-white rounded p-3">
             <form onSubmit={Submit}>
-            <h1 className="text-center">Event Application </h1>
+            <h1 className="text-center">Event Web Application </h1>
                 <h2>Add User</h2>
                 <div className="mb-2">
                    <label htmlFor="">Name</label>
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -24,7 +24,7 @@ function Users() {
   return ( 
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-      <h1 className="text-center">Event Application </h1>
+      <h1 className="text-center">Event Web Application </h1>
         <Link to="/create" className="btn btn-success">Add +</Link>
         <table className="table">
           <thead>
