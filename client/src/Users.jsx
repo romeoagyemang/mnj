24e8150211a866849c1a2af20df0a7d00cf58632
romeoagyemang <romeoasiamah79@gmail.com>
2023-12-25 00:39:00 -0600

    second commit

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -21,9 +21,7 @@ function Users() {
     .catch(err => console.log(err))
   }
 
-  return (
-    
-     
+  return ( 
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
       <h1 className="text-center">Event Application </h1>
